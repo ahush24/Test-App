84eb13c3852247b9b4e01d5f67dc267eae513363
ahush24 <41281462+ahush24@users.noreply.github.com>
2021-05-29 16:09:07 +0530

    Update settings.py

--- testpress/settings.py
+++ testpress/settings.py
@@ -120,14 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/image/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/image/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 # Default primary key field type
